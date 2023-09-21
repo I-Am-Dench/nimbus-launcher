e86b30228f0c7892901236b6336813cb850cd5bd
I-Am-Dench <mrantistupid@gmail.com>
2023-09-21 01:41:56 -0400

    Implemented localdata for linux

---
+++ resource/localdata_linux.go
@@ -0,0 +1,20 @@
+//go:build linux
+// +build linux
+
+package resource
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func DefaultApplicationsDirectory() string {
+	homeDirectory, err := os.UserHomeDir()
+	if err != nil {
+		log.Printf("cannot find home directory: %v\n", err)
+		log.Println("Using ~/")
+		return "~/"
+	}
+	return filepath.Join(homeDirectory, "games", DEFAULT_DIR_CLIENT)
+}
