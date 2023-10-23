5d6bd631c09c8374f930e6900279c0c25b0aa3ce
Theodore <mrantistupid@gmail.com>
2023-10-22 21:56:44 -0400

    Added flags to remove windows shell when running binary

--- release_win.bat
+++ release_win.bat
@@ -1,7 +1,7 @@
 if exist releases\build rmdir releases\build /s /q
 if not exist releases\build md releases\build
 
-go build -o lu-launcher.exe
+go build -ldflags -H=windowsgui -o lu-launcher.exe
 
 copy lu-launcher.exe releases\build
 robocopy assets releases\build\assets /e /mir /np /nfl
