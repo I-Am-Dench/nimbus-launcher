b3ca12a82ba40557252b3b4974592c50e02242f8
Theodore <mrantistupid@gmail.com>
2024-01-09 16:40:20 -0500

    Changed copy to move when relocating launcher build into launcher folder

--- release/release_win.bat
+++ release/release_win.bat
@@ -4,6 +4,6 @@ if exist launcher rmdir launcher /s /q
 if not exist launcher md launcher
 
 go build -ldflags -H=windowsgui -o lu-launcher.exe ..
-copy lu-launcher.exe launcher
+move lu-launcher.exe launcher
 
 7z a -r ../v0.1.0-win.zip launcher
