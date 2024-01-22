6f38cf2e3bf1a5f92875f2ddf23c67b831617b39
Theodore <mrantistupid@gmail.com>
2024-01-21 21:38:14 -0500

    Added basic patch unit test

---
+++ resource/patch/environment_test.go
@@ -0,0 +1,119 @@
+package patch_test
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/I-Am-Dench/lu-launcher/ldf"
+	"github.com/I-Am-Dench/lu-launcher/resource/server"
+)
+
+type environment struct {
+	Dir string
+
+	PatchServer  *http.Server
+	ServerConfig *server.Server
+}
+
+type fileSystem map[string][]byte
+
+func (fs fileSystem) Init(dir string, t *testing.T) {
+	t.Helper()
+
+	err := os.RemoveAll(dir)
+	if err != nil {
+		t.Fatalf("init fs: %v", err)
+	}
+
+	for relativePath, data := range fs {
+		path := filepath.Join(dir, relativePath)
+		t.Logf("creating client file: %s", path)
+
+		err := os.MkdirAll(filepath.Dir(path), 0755)
+		if err != nil {
+			t.Fatalf("init fs: %v", err)
+		}
+
+		err = os.WriteFile(path, data, 0755)
+		if err != nil {
+			t.Fatalf("init fs: %v", err)
+		}
+	}
+}
+
+func newPatchServer(t *testing.T, ctx context.Context, fs fileSystem) *http.Server {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		t.Logf("[PATCH SERVER] {%s} %s", r.Method, r.URL.Path)
+
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		data, ok := fs[r.URL.Path]
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(data)
+	})
+
+	return &http.Server{
+		Addr:    "127.0.0.1:3000",
+		Handler: mux,
+		BaseContext: func(l net.Listener) context.Context {
+			return ctx
+		},
+	}
+}
+
+func newServerConfiguration(dir string) *server.Server {
+	bootDir := filepath.Join(dir, "boot")
+	patchDir := filepath.Join(dir, "patches")
+
+	return server.New(bootDir, patchDir, "", "", "http", &ldf.BootConfig{
+		PatchServerIP:   "127.0.0.1",
+		PatchServerPort: 3000,
+		PatchServerDir:  "patches",
+	})
+}
+
+func setup(t *testing.T, serverFS fileSystem) (*environment, func()) {
+	t.Helper()
+
+	dir, err := os.MkdirTemp(".", "patch_test*.tmp")
+	if err != nil {
+		t.Fatalf("setup: %v", err)
+	}
+
+	t.Logf("Using temp dir \"%s\"", dir)
+
+	env := &environment{
+		Dir: dir,
+	}
+
+	ctx := context.Background()
+	env.PatchServer = newPatchServer(t, ctx, serverFS)
+	env.PatchServer.RegisterOnShutdown(func() {
+		t.Logf("Patch server shutdown.")
+	})
+
+	env.ServerConfig = newServerConfiguration(dir)
+
+	return env, func() {
+		env.PatchServer.Shutdown(ctx)
+
+		err := os.RemoveAll(dir)
+		if err != nil {
+			t.Log(err)
+		}
+	}
+}
---
+++ resource/patch/patch_test.go
@@ -0,0 +1,190 @@
+package patch_test
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/I-Am-Dench/lu-launcher/client"
+	"github.com/I-Am-Dench/lu-launcher/resource/patch"
+	"github.com/I-Am-Dench/lu-launcher/resource/server"
+)
+
+type cache struct {
+	m map[string]client.ClientResource
+}
+
+func (cache *cache) Add(resource client.ClientResource) error {
+	cache.m[resource.Path] = resource
+	return nil
+}
+
+func (cache *cache) Get(path string) (client.ClientResource, error) {
+	resource, ok := cache.m[path]
+	if !ok {
+		return client.ClientResource{}, fmt.Errorf("cache: \"%s\" does not exist", path)
+	}
+
+	return resource, nil
+}
+
+func (cache *cache) GetResources() ([]client.ClientResource, error) {
+	resources := []client.ClientResource{}
+	for _, resource := range cache.m {
+		resources = append(resources, resource)
+	}
+	return resources, nil
+}
+
+func (cache *cache) Has(path string) bool {
+	_, ok := cache.m[path]
+	return ok
+}
+
+func (cache *cache) Close() error {
+	return nil
+}
+
+var testPatch = `{
+	"download": [
+		{
+			"path": "/v1.0.0/a",
+			"name": "a"
+		},
+		{
+			"path": "/v1.0.0/b",
+			"name": "b"
+		},
+		{
+			"path": "/v1.0.0/c",
+			"name": "c"
+		}
+	],
+	"replace": {
+		"a": "data/file1",
+		"b": "data/file2",
+		"c": "data/file3"
+	}
+}`
+
+var testVersions = server.PatchesSummary{
+	CurrentVersion:   "v1.0.0",
+	PreviousVersions: []string{},
+}
+
+func serverFileSystem() fileSystem {
+	fs := make(fileSystem)
+
+	versions, _ := json.Marshal(testVersions)
+	fs["/patches"] = versions
+
+	fs["/patches/v1.0.0"] = []byte(testPatch)
+
+	fs["/patches/v1.0.0/a"] = []byte("Test 1")
+	fs["/patches/v1.0.0/b"] = []byte("Test 2")
+	fs["/patches/v1.0.0/c"] = []byte("Test 3")
+
+	return fs
+}
+
+func clientFileSystem() fileSystem {
+	fs := make(fileSystem)
+
+	fs["data/file1"] = []byte("default data 1")
+	fs["data/file2"] = []byte("default data 2")
+	fs["data/file3"] = []byte("default data 3")
+
+	return fs
+}
+
+func expectedClient() fileSystem {
+	fs := make(fileSystem)
+
+	fs["data/file1"] = []byte("Test 1")
+	fs["data/file2"] = []byte("Test 2")
+	fs["data/file3"] = []byte("Test 3")
+
+	return fs
+}
+
+func hasSameContent(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func checkContents(dir, path string, expected []byte) error {
+	actual, err := os.ReadFile(filepath.Join(dir, path))
+	if err != nil {
+		return fmt.Errorf("check contents: %v", err)
+	}
+
+	if !hasSameContent(expected, actual) {
+		return fmt.Errorf("expected `%v` but read `%v`", expected, actual)
+	}
+
+	return nil
+}
+
+func TestPatching(t *testing.T) {
+	serverFS := serverFileSystem()
+	clientFS := clientFileSystem()
+
+	env, teardown := setup(t, serverFS)
+	defer teardown()
+
+	clientDirectory := filepath.Join(env.Dir, "client")
+
+	rejections := patch.NewRejectionList(filepath.Join(env.Dir, "rejections.json"))
+	clientCache := &cache{
+		m: make(map[string]client.ClientResource),
+	}
+
+	_, err := env.ServerConfig.GetPatch("v1.0.0")
+	if !errors.Is(err, patch.ErrPatchesUnavailable) {
+		t.Fatalf("test patching: Server.GetPatch did not return patch.ErrPatchesUnavailable: instead: %v", err)
+	}
+
+	go env.PatchServer.ListenAndServe()
+
+	t.Log("Started test patch server.")
+
+	clientFS.Init(clientDirectory, t)
+
+	patch, err := env.ServerConfig.GetPatch("v1.0.0")
+	if err != nil {
+		t.Fatalf("test patching: %v", err)
+	}
+
+	err = patch.UpdateResources(env.ServerConfig, rejections)
+	if err != nil {
+		t.Fatalf("test patching: update resources: %v", err)
+	}
+
+	err = patch.TransferResources(clientDirectory, clientCache, env.ServerConfig)
+	if err != nil {
+		t.Fatalf("test patching: transfer resources: %v", err)
+	}
+
+	expected := expectedClient()
+
+	for path, expectedData := range expected {
+		err := checkContents(clientDirectory, path, expectedData)
+		if err != nil {
+			t.Errorf("test patching: %v", err)
+		} else {
+			t.Logf("\"%s\" is correct!", path)
+		}
+	}
+}
