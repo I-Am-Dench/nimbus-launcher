023fe04a04b57befeed4b8f6a8c0433c655310df
Theodore <mrantistupid@gmail.com>
2024-06-29 21:24:29 -0400

    Added license

--- app/nlwindows/info.go
+++ app/nlwindows/info.go
@@ -1,7 +1,11 @@
 package nlwindows
 
 import (
+	"log"
 	"net/url"
+	"os"
+	"os/exec"
+	"path"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -19,6 +23,20 @@ func mustParse(rawUrl string) *url.URL {
 	return url
 }
 
+func OpenLicense() {
+	dir := "."
+	if exe, err := os.Executable(); err == nil {
+		dir = path.Dir(exe)
+	}
+
+	// Only for windows -- Needs `open -t [or -e]` for Mac or equivalent for Linux
+	cmd := exec.Command("notepad", path.Join(dir, "LICENSE"))
+	cmd.Stderr = os.Stdout
+
+	log.Println("OpenLicense:", cmd)
+	cmd.Run()
+}
+
 func NewInfoWindow(app fyne.App) fyne.Window {
 	window := app.NewWindow("Info")
 	window.SetIcon(theme.InfoIcon())
@@ -31,12 +49,23 @@ func NewInfoWindow(app fyne.App) fyne.Window {
 		container.NewPadded(
 			container.NewVBox(
 				heading, widget.NewSeparator(),
-				widget.NewForm(
-					widget.NewFormItem("Version", widget.NewLabel(version.Get().Name())),
-					widget.NewFormItem("Revision", widget.NewLabel(version.Revision())),
-					widget.NewFormItem("Author", widget.NewLabel("Theodore Friedrich")),
-					widget.NewFormItem("Source", widget.NewHyperlink("https://github.com/I-Am-Dench/lu-launcher", mustParse("https://github.com/I-Am-Dench/lu-launcher"))),
+				container.NewHBox(
+					widget.NewForm(
+						widget.NewFormItem("Version", widget.NewLabel(version.Get().Name())),
+						widget.NewFormItem("Revision", widget.NewLabel(version.Revision())),
+						widget.NewFormItem("Author", widget.NewLabel("I-Am-Dench")),
+						widget.NewFormItem("Source", widget.NewHyperlink("https://github.com/I-Am-Dench/lu-launcher", mustParse("https://github.com/I-Am-Dench/lu-launcher"))),
+						widget.NewFormItem("License", widget.NewButton("GNU GPLv3", OpenLicense)),
+					),
+					container.NewStack(
+						container.NewVBox(
+							widget.NewLabel("Copyright (C) 2023 Theodore Friedrich (I-Am-Dench)"),
+							widget.NewLabel("This program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version."),
+							widget.NewLabel("This program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\nGNU General Public License for more details."),
+						),
+					),
 				),
+				widget.NewLabel("You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>."),
 			),
 		),
 	)
--- release/release_win.bat
+++ release/release_win.bat
@@ -3,7 +3,7 @@ cd %~dp0
 if exist launcher rmdir launcher /s /q
 if not exist launcher md launcher
 
-go build -tags release -ldflags -H=windowsgui -o lu-launcher.exe ..
-move lu-launcher.exe launcher
+go build -tags release -ldflags -H=windowsgui -o .\launcher\lu-launcher.exe ..
+copy ..\LICENSE .\launcher
 
 7z a -r ../launcher-win.zip launcher
