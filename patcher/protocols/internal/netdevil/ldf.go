35c7ffbaccd326345f2484f758fbff770de96ee9
Theodore <mrantistupid@gmail.com>
2026-06-19 23:30:16 -0400

    Refactored LdfEntries unmarshal function

--- patcher/protocols/internal/netdevil/ldf.go
+++ patcher/protocols/internal/netdevil/ldf.go
@@ -2,6 +2,8 @@ package netdevil
 
 import (
 	"encoding/xml"
+	"errors"
+	"io"
 
 	"github.com/I-Am-Dench/goverbuild/encoding/ldf"
 )
@@ -17,68 +19,85 @@ func (l LdfEntries) Map() ldf.Map {
 }
 
 func (l *LdfEntries) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
-	type KeyValue struct {
-		Id    string `xml:"id,attr"`
-		Value []byte `xml:",chardata"`
+	entries := []ldf.Entry{}
+
+	type keyValue struct {
+		XMLName xml.Name
+		Id      string `xml:"id,attr"`
+		Value   []byte `xml:",chardata"`
 	}
 
-	appendEntries := func(entries *[]ldf.Entry, kvs []KeyValue, valueType ldf.ValueType) error {
-		for _, kv := range kvs {
-			entry, err := ldf.Token{
-				Key:   kv.Id,
-				Type:  valueType,
-				Value: kv.Value,
-			}.Entry()
+	toToken := func(kv keyValue) (ldf.Token, bool) {
+		var t ldf.ValueType
+		switch kv.XMLName.Local {
+		case "string":
+			t = ldf.ValueTypeString
+		case "utf8":
+			t = ldf.ValueTypeUtf8
+		case "int32":
+			t = ldf.ValueTypeI32
+		case "uint32":
+			t = ldf.ValueTypeU32
+		case "int64":
+			t = ldf.ValueTypeI64
+		case "uint64":
+			t = ldf.ValueTypeU64
+		case "float32":
+			t = ldf.ValueTypeFloat
+		case "float64":
+			t = ldf.ValueTypeDouble
+		case "bool":
+			t = ldf.ValueTypeBool
+		default:
+			return ldf.Token{}, false
+		}
+
+		return ldf.Token{
+			Key:   kv.Id,
+			Type:  t,
+			Value: kv.Value,
+		}, true
+	}
+
+loop:
+	for {
+		t, err := d.Token()
+		if errors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+
+		switch token := t.(type) {
+		case xml.StartElement:
+			var kv keyValue
+			err := d.DecodeElement(&kv, &token)
+			if errors.Is(err, io.EOF) {
+				break loop
+			}
+
 			if err != nil {
 				return err
 			}
-			*entries = append(*entries, entry)
+
+			tok, ok := toToken(kv)
+			if !ok {
+				continue
+			}
+
+			entry, err := tok.Entry()
+			if err != nil {
+				return err
+			}
+
+			entries = append(entries, entry)
+		case xml.EndElement:
+			if token.Name.Local == start.Name.Local {
+				break
+			}
 		}
-		return nil
-	}
-
-	keys := struct {
-		Strings  []KeyValue `xml:"string"`
-		Utf8s    []KeyValue `xml:"utf8"`
-		Int32s   []KeyValue `xml:"int32"`
-		Uint32s  []KeyValue `xml:"uint32"`
-		Int64s   []KeyValue `xml:"int64"`
-		Uint64s  []KeyValue `xml:"uint64"`
-		Float32s []KeyValue `xml:"float32"`
-		Float64s []KeyValue `xml:"float64"`
-		Bools    []KeyValue `xml:"bool"`
-	}{}
-	if err := d.DecodeElement(&keys, &start); err != nil {
-		return err
-	}
-
-	var entries []ldf.Entry
-	if err := appendEntries(&entries, keys.Strings, ldf.ValueTypeString); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Utf8s, ldf.ValueTypeUtf8); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Int32s, ldf.ValueTypeI32); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Uint32s, ldf.ValueTypeU32); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Int64s, ldf.ValueTypeI64); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Uint64s, ldf.ValueTypeU64); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Float32s, ldf.ValueTypeFloat); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Float64s, ldf.ValueTypeDouble); err != nil {
-		return err
-	}
-	if err := appendEntries(&entries, keys.Bools, ldf.ValueTypeBool); err != nil {
-		return err
 	}
 	*l = entries
 
